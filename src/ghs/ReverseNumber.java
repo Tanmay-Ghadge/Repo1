71b79bb32fa35f16e482c56a7e4bbe2897a7cf36
Tanmay-Ghadge <115142117+Tanmay-Ghadge@users.noreply.github.com>
2023-07-17 20:07:57 +0530

    merging this first

--- src/ghs/ReverseNumber.java
+++ src/ghs/ReverseNumber.java
@@ -8,6 +8,10 @@ public class ReverseNumber {
 		int number= 0;
 		int temp;
 // this line is not in my local
+		//line 1
+		//line 2
+		//line 3
+		//line 1,2,3 are not in cinflict branch
 		
 		while(numberToRverse>0)
 		{
