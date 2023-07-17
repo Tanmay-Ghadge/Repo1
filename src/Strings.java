a8cd0a6da3363866660ed7e1c8ba5dcf40e8bd58
Tanmay <tanmayghadge11@gmail.com>
2023-07-17 15:53:11 +0530

    chaanged string programm

--- src/Strings.java
+++ src/Strings.java
@@ -5,19 +5,19 @@ public class Strings {
 	
 	public static void main(String[] args) 
 	{
-//		String s =new String("tanmay");
-//		System.out.println("before hashcode-"+s.hashCode());
-//		s=s.concat("ghadge");
-//		System.out.println("after hashcode-"+s.hashCode());
-//
-//		System.out.println(s);
-
-		StringBuffer s =new StringBuffer("tanmay");
+		String s =new String("tanmay");
 		System.out.println("before hashcode-"+s.hashCode());
-		s=s.append("ghadge");
+		s=s.concat("ghadge");
 		System.out.println("after hashcode-"+s.hashCode());
 
 		System.out.println(s);
+
+		StringBuffer s1 =new StringBuffer("tanmay");
+		System.out.println("before hashcode-"+s1.hashCode());
+		s1=s1.append("ghadge");
+		System.out.println("after hashcode-"+s1.hashCode());
+
+		System.out.println(s1);
 		
 		
 	}
